c542b7599fc4918f12c39c222ae70ac77caf345b
RKSM-GIT <m.r.k.s@iitg.ac.in>
2024-11-24 19:08:41 +0530

    just learning...

---
+++ zero2prod/target/debug/deps/zero2prod-512eb0f499d7e7d0.d
@@ -0,0 +1,5 @@
+C:\Users\rksme\Desktop\Rust Projects\learn-rust\zero2prod\target\debug\deps\zero2prod-512eb0f499d7e7d0.exe: src/main.rs
+
+C:\Users\rksme\Desktop\Rust Projects\learn-rust\zero2prod\target\debug\deps\zero2prod-512eb0f499d7e7d0.d: src/main.rs
+
+src/main.rs:
---
+++ zero2prod/target/debug/deps/zero2prod-8ace78444e474684.d
@@ -0,0 +1,9 @@
+C:\Users\rksme\Desktop\Rust Projects\learn-rust\zero2prod\target\debug\deps\libzero2prod-8ace78444e474684.rmeta: src/main.rs Cargo.toml
+
+C:\Users\rksme\Desktop\Rust Projects\learn-rust\zero2prod\target\debug\deps\zero2prod-8ace78444e474684.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
